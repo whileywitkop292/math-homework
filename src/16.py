90188ca794bf39fd92efa160b3ce919c29d385b2
whileywitkop292 <whileywitkop292@gmail.com>
2025-03-21 08:51:13 +0000

    Add 16.py

---
+++ src/16.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def add_numbers(a, b):
+    """
+    Add two numbers.
+    
+    Args:
+    a (float): The first number.
+    b (float): The second number.
+    
+    Returns:
+    float: The sum of the two numbers.
+    """
+    return a + b
+
+print(add_numbers(3.5, 2.0))
